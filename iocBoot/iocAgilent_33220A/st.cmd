364724db7b199682f099a435a1c81d83376fd101
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-06-19 07:46:09 +0000

    Ticket 112. Minor changes to Makefile and st.cmd
    Former-commit-id: 87d2eac2985d6e7b3712d8efb14b4f80dc9d66fe

--- iocBoot/iocAgilent_33220A/st.cmd
+++ iocBoot/iocAgilent_33220A/st.cmd
@@ -12,10 +12,10 @@ dbLoadDatabase "dbd/Agilent_33220A.dbd"
 Agilent_33220A_registerRecordDeviceDriver pdbbase
 
 lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/Agilent_33220A.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
+#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/Agilent_33220A.xml", "", 6)
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/Agilent_33220A.db","P=AG53220A:")
-
+dbLoadRecords("$(TOP)/db/Agilent_33220A.db","P=AG33220A:")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
