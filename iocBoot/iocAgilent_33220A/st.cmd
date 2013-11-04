554f917d8f6ddf2adf53c4695dae0fec7986ccc2
Matt Clarke <matt.clarke@stfc.ac.uk>
2013-11-04 10:50:23 +0000

    Refs ticket 221. Fixed agilent 33220A as it was not working (though it was building!). Also, added pvdump.
    Former-commit-id: 935e92455ae139ca2d0b38692497c56613398935

--- iocBoot/iocAgilent_33220A/st.cmd
+++ iocBoot/iocAgilent_33220A/st.cmd
@@ -14,12 +14,17 @@ cd ${TOP}
 dbLoadDatabase "dbd/Agilent_33220A.dbd"
 Agilent_33220A_registerRecordDeviceDriver pdbbase
 
-lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/Agilent_33220A.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
-#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/Agilent_33220A.xml", "", 6)
+
+
+lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/agilent_33220A.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
+#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/Agilent_33220AApp/protocol/agilent_33220A.xml", "", 6)
 
 ## Load record instances
-dbLoadRecords("$(TOP)/db/Agilent_33220A.db","P=$(IOCNAME):")
+dbLoadRecords("db/Agilent_33220A.db","P=$(IOCNAME):")
 dbLoadRecords("$(IOCSTATS_DB)","IOC=$(IOCNAME)")
+
+pvdump("../pvdump/PV_data.db", "$(IOCNAME)")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
+
